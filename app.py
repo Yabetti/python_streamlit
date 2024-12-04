3b0963661539ce5714943543817b857d37d6bf26
Yabetti <teemomickey@yahoo.co.jp>
2024-12-04 17:45:29 +0900

    モジュール経由のテキストボックス作成

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import streamlit as st
+import func as f
 
 input_num = st.number_input('Input a number', value=0)
 
 result = input_num ** 2
 st.write('Result: ', result)
+
+
+# テキストボックスの表示
+user_input = st.text_input("文字を入力してください:", "")
+
+# 入力された文字を表示
+if user_input:
+    st.write(f"入力された文字: {f.addInputStr(user_input)}")
--- func.py
+++ func.py
@@ -2,3 +2,6 @@ def doubleValue(x):
     print(x+x)
     
     return 4
+
+def addInputStr(y):
+    return "モジュールからの返り値"+y
